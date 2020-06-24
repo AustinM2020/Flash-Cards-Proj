b339a42c180b992f7c770a72b9a6aef19b6697f5
Austin Moen <moenaustin20@gmail.com>
2020-06-24 14:42:39 -0500

    You can choose a stack to add a new card to

--- src/components/create.js
+++ src/components/create.js
@@ -3,31 +3,77 @@ import axios from 'axios';
 
 class Create extends React.Component {
     state = {
-        stackId: 0,
         word: " ",
-        definition: " "
+        definition: " ",
+        stackCategory: " ",
+        categories: []
     }
+
+    componentDidMount () {
+        var items = [];
+        axios.get('https://localhost:44393/api/collection')
+          .then(res => {
+            let categories = this.state.categories;
+            for(var i = 0; i < res.data.length; i++){
+                categories.push(res.data[i].title)
+            }
+            this.setState({ categories })
+            console.log(items)
+          })
+      }
     
-    addCard(){
+    addCard = () => {
+        var stackId = 0;
+        for(var i = 0; i < this.state.categories.length; i++){
+            if(this.state.stackCategory == this.state.categories[i]){
+                stackId = i + 1;
+            }
+        }
+        console.log(stackId)
         axios.post('https://localhost:44393/api/card', {
-            "stackId": this.state.stackId,
-            "word": this.state.word,
-            "definition": this.state.definition
+            "stackId": stackId,
+            "word": `${this.state.word}`,
+            "definition": `${this.state.definition}`
         })
         .then(function (response) {
             console.log(response);
         })
     }
 
-    render = () => {
+    handleWordChange = (event) => {
+        this.setState({
+            word: event.target.value
+        })
+    }
+
+    handleDefChange = (event) => {
+        this.setState({
+            definition: event.target.value
+        })
+    }
+
+    handleStackChange = (event) => {
+        this.setState({
+            stackCategory : event.target.value
+        })
+    }
+
+    render() {
+        console.log(this.state.categories)
         return (
           <div>
-              <form onSubmit={this.addCard}>
-                <input value={this.state.stackId}></input><br></br>
-                <input value={this.state.word}></input><br></br>
-                <input value={this.state.definition}></input><br></br>
-              </form>
-              <button type="submit" onClick={this.addCard}>Add New Word</button>
+            <select value={this.state.stackCategory} onChange={this.handleStackChange}>
+                <option>Select</option>
+                {this.state.categories.map(category => (
+                    <option key={category} value={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
+            <br></br>
+            <input type="text" value={this.state.word} onChange={this.handleWordChange}></input><br></br>
+            <input type="text" value={this.state.definition} onChange={this.handleDefChange}></input><br></br>
+            <button type="submit" onClick={this.addCard}>Add New Word</button>
           </div>
         )
       }
